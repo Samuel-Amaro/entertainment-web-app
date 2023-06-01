f1247a3c1fefc23c7b0bea613cd3696c3e417bfb
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-01 14:28:18 -0300

    feat: adicionando skeleton de loading de ui para page movie segmento dinamico, e adicionando estilos mobile-first

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -18,6 +18,7 @@ import styles from "./movie.module.css";
 import { Metadata, ResolvingMetadata } from "next";
 import IconLink from "@/components/Icons/IconLink";
 import PlayerVideo from "@/components/PlayerVideo";
+import SkeletonPageMovie from "@/components/Skeletons/PageMovie";
 
 type Props = {
   params: { id: number };
@@ -69,7 +70,6 @@ export async function generateMetadata(
   };
 }
 
-
 //TODO: apos estilização criar um skeleton de UI para a loading page
 
 export default async function Page({ params }: Props) {
@@ -78,120 +78,122 @@ export default async function Page({ params }: Props) {
   const listOfLanguages = await getListOfLanguages();
 
   return (
-    <main className={styles.main}>
-      <section className={styles.sectionSummary}>
-        <div className={styles.wrapperImagePoster}>
-          <Image
-            src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${detailsMovie.poster_path}`}
-            alt={`poster movie ${detailsMovie.title}`}
-            placeholder="blur"
-            blurDataURL={`data:image/svg+xml;base64,${toBase64(
-              shimer(240, 140)
-            )}`}
-            width={300}
-            height={300}
-            /*fill={true}*/
-            className={styles.posterImage}
-            title={`poster movie ${detailsMovie.title}`}
-          />
-        </div>
-        <div>
-          <h1 className={`headingL ${styles.title}`}>
-            <span className={styles.titleText}>{detailsMovie.title}</span>{" "}
-            <span className={styles.titleYear}>{`(${new Date(
-              detailsMovie.release_date
-            ).getFullYear()})`}</span>
-          </h1>
-          <p className={styles.metadatas}>
-            <span className={styles.metadataDate}>
-              {detailsMovie.release_date}
-            </span>
-            <span className={styles.diviser}></span>
-            <span className={styles.metadataGenres}></span>
-            {detailsMovie.genres.map((genre) => genre.name).join(",")}
-            <span className={styles.diviser}></span>
-            <span className={styles.metadataRuntime}>
-              {convertMinutesInHours(detailsMovie.runtime)}
-            </span>
-          </p>
-          <PlayerVideo idMovie={params.id} />
-          <div>
-            <em className={styles.tagline}>{detailsMovie.tagline}</em>
-            <h2 className={`headingM ${styles.subtitle}`}>Overview</h2>
-            <p className={styles.overview}>{detailsMovie.overview}</p>
+    <>
+      <main className={styles.main}>
+        <section className={styles.sectionSummary}>
+          <div className={styles.wrapperImagePoster}>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${detailsMovie.poster_path}`}
+              alt={`poster movie ${detailsMovie.title}`}
+              placeholder="blur"
+              blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                shimer(240, 140)
+              )}`}
+              width={300}
+              height={300}
+              /*fill={true}*/
+              className={styles.posterImage}
+              title={`poster movie ${detailsMovie.title}`}
+            />
           </div>
-        </div>
-      </section>
-      <section className={styles.sectionInfo}>
-        <div className={styles.containerInfo}>
-          <p className={styles.info}>
-            <span className={styles.data}>Status</span>
-            <span className={styles.valueData}>{detailsMovie.status}</span>
-          </p>
-          <p className={styles.info}>
-            <span className={styles.data}>Original Language</span>
-            <span className={styles.valueData}>
-              {getLanguage(detailsMovie.original_language, listOfLanguages)}
-            </span>
-          </p>
-          <p className={styles.info}>
-            <span className={styles.data}>Original title</span>
-            <span className={styles.valueData}>
-              {detailsMovie.original_title}
-            </span>
-          </p>
-          <p className={styles.info}>
-            <span className={styles.data}>Budget</span>
-            <span className={styles.valueData}>
-              {formatNumber(detailsMovie.budget)}
-            </span>
-          </p>
-          <p className={styles.info}>
-            <span className={styles.data}>Revenue</span>
-            <span className={styles.valueData}>
-              {formatNumber(detailsMovie.revenue)}
-            </span>
-          </p>
-        </div>
-        <Link
-          href={detailsMovie.homepage}
-          target="_blank"
-          rel="external"
-          title="Visit Home Page"
-          className={styles.linkHomePage}
-        >
-          <IconLink className={styles.iconLink} />
-        </Link>
-      </section>
-      <section>
-        <h2 className={`headingM ${styles.subtitle}`}>Casts</h2>
-        <ul className={styles.list}>
-          {creditsMovie.cast.map((cast) => (
-            <li key={cast.id} className={styles.card}>
-              <div className={styles.wrapperProfile}>
-                {cast.profile_path && (
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${cast.profile_path}`}
-                    alt={`Profile ${cast.original_name} with character ${cast.character}`}
-                    placeholder="blur"
-                    blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                      shimer(240, 140)
-                    )}`}
-                    width={150}
-                    height={150}
-                    title={`Profile ${cast.original_name} with character ${cast.character}`}
-                    className={styles.profileImg}
-                  />
-                )}
-              </div>
-              <div className={styles.datasCard}>
-                <p className={styles.originalName}>{cast.original_name}</p>
-                <p className={styles.character}>{cast.character}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </main>
+          <div>
+            <h1 className={`headingL ${styles.title}`}>
+              <span className={styles.titleText}>{detailsMovie.title}</span>{" "}
+              <span className={styles.titleYear}>{`(${new Date(
+                detailsMovie.release_date
+              ).getFullYear()})`}</span>
+            </h1>
+            <p className={styles.metadatas}>
+              <span className={styles.metadataDate}>
+                {detailsMovie.release_date}
+              </span>
+              <span className={styles.diviser}></span>
+              <span className={styles.metadataGenres}></span>
+              {detailsMovie.genres.map((genre) => genre.name).join(",")}
+              <span className={styles.diviser}></span>
+              <span className={styles.metadataRuntime}>
+                {convertMinutesInHours(detailsMovie.runtime)}
+              </span>
+            </p>
+            <PlayerVideo idMovie={params.id} />
+            <div>
+              <em className={styles.tagline}>{detailsMovie.tagline}</em>
+              <h2 className={`headingM ${styles.subtitle}`}>Overview</h2>
+              <p className={styles.overview}>{detailsMovie.overview}</p>
+            </div>
+          </div>
+        </section>
+        <section className={styles.sectionInfo}>
+          <div className={styles.containerInfo}>
+            <p className={styles.info}>
+              <span className={styles.data}>Status</span>
+              <span className={styles.valueData}>{detailsMovie.status}</span>
+            </p>
+            <p className={styles.info}>
+              <span className={styles.data}>Original Language</span>
+              <span className={styles.valueData}>
+                {getLanguage(detailsMovie.original_language, listOfLanguages)}
+              </span>
+            </p>
+            <p className={styles.info}>
+              <span className={styles.data}>Original title</span>
+              <span className={styles.valueData}>
+                {detailsMovie.original_title}
+              </span>
+            </p>
+            <p className={styles.info}>
+              <span className={styles.data}>Budget</span>
+              <span className={styles.valueData}>
+                {formatNumber(detailsMovie.budget)}
+              </span>
+            </p>
+            <p className={styles.info}>
+              <span className={styles.data}>Revenue</span>
+              <span className={styles.valueData}>
+                {formatNumber(detailsMovie.revenue)}
+              </span>
+            </p>
+          </div>
+          <Link
+            href={detailsMovie.homepage}
+            target="_blank"
+            rel="external"
+            title="Visit Home Page"
+            className={styles.linkHomePage}
+          >
+            <IconLink className={styles.iconLink} />
+          </Link>
+        </section>
+        <section>
+          <h2 className={`headingM ${styles.subtitle}`}>Casts</h2>
+          <ul className={styles.list}>
+            {creditsMovie.cast.map((cast) => (
+              <li key={cast.id} className={styles.card}>
+                <div className={styles.wrapperProfile}>
+                  {cast.profile_path && (
+                    <Image
+                      src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${cast.profile_path}`}
+                      alt={`Profile ${cast.original_name} with character ${cast.character}`}
+                      placeholder="blur"
+                      blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                        shimer(240, 140)
+                      )}`}
+                      width={150}
+                      height={150}
+                      title={`Profile ${cast.original_name} with character ${cast.character}`}
+                      className={styles.profileImg}
+                    />
+                  )}
+                </div>
+                <div className={styles.datasCard}>
+                  <p className={styles.originalName}>{cast.original_name}</p>
+                  <p className={styles.character}>{cast.character}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </>
   );
 }
---
+++ src/components/Skeletons/PageMovie/index.tsx
@@ -0,0 +1,64 @@
+import styles from "./styles.module.css";
+
+export default function SkeletonPageMovie() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.sectionSummary}>
+        <div className={styles.poster}></div>
+        <div>
+          <h1 className={styles.title}></h1>
+          <p className={styles.metadatas}></p>
+          <p className={styles.playerVideo}></p>
+          <div>
+            <p className={styles.tagline}></p>
+            <h2 className={styles.subtitle}></h2>
+            <p className={styles.overview}></p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.sectionInfo}>
+        <div className={styles.containerInfo}>
+          <p className={styles.data}></p>
+          <p className={styles.data}></p>
+          <p className={styles.data}></p>
+          <p className={styles.data}></p>
+          <p className={styles.data}></p>
+          <p className={styles.data}></p>
+        </div>
+        <p className={styles.link}></p>
+      </div>
+      <div>
+        <h2 className={styles.subtitle}></h2>
+        <ul className={styles.list}>
+          <li className={styles.card}>
+            <div>
+              <div className={styles.wrapperProfile}></div>
+              <div className={styles.datasCard}>
+                <p className={styles.originalName}></p>
+                <p className={styles.character}></p>
+              </div>
+            </div>
+          </li>
+          <li className={styles.card}>
+            <div>
+              <div className={styles.wrapperProfile}></div>
+              <div className={styles.datasCard}>
+                <p className={styles.originalName}></p>
+                <p className={styles.character}></p>
+              </div>
+            </div>
+          </li>
+          <li className={styles.card}>
+            <div>
+              <div className={styles.wrapperProfile}></div>
+              <div className={styles.datasCard}>
+                <p className={styles.originalName}></p>
+                <p className={styles.character}></p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
