fdc8bddcf7c36694f32dddedcf4757042ac5e7f1
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-06 17:35:49 -0300

    feat: adicionando estilos mobile-first na página detalhes de tv series

--- src/app/tv/[id]/page.tsx
+++ src/app/tv/[id]/page.tsx
@@ -10,10 +10,13 @@ import { getLanguage, getVideoTrailer, shimer, toBase64 } from "@/utils";
 import { Metadata, ResolvingMetadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./tvdetails.module.css";
 
 type Props = {
   params: { id: number };
 };
+
+//TODO: add styles mobile-first
 
 export async function generateMetadata(
   { params }: Props,
@@ -69,9 +72,9 @@ export default async function Page({ params }: Props) {
   const trailer = getVideoTrailer(listOfVideos.results);
 
   return (
-    <main>
-      <section>
-        <div>
+    <main className={styles.main}>
+      <section className={styles.sectionSummary}>
+        <div className={styles.wrapperImagePoster}>
           <Image
             src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${detailsTvSeries.poster_path}`}
             alt={`poster tv series ${detailsTvSeries.original_name}`}
@@ -82,58 +85,70 @@ export default async function Page({ params }: Props) {
             width={300}
             height={300}
             title={`poster tv series ${detailsTvSeries.original_name}`}
+            className={styles.posterImage}
           />
         </div>
         <div>
-          <h1>
-            <span>{detailsTvSeries.original_name}</span>
+          <h1 className={`headingL ${styles.title}`}>
+            <span>{detailsTvSeries.original_name}</span>{" "}
             <span>{`(${new Date(
               detailsTvSeries.first_air_date
             ).getFullYear()})`}</span>
           </h1>
-          <p>
-            <span>
+          <p className={styles.metadatas}>
+            <span className={styles.metadataGenres}>
               {detailsTvSeries.genres.map((genre) => genre.name).join(",")}
             </span>
           </p>
           {trailer && <PlayerVideo video={trailer} />}
-          <div>
-            <em>{detailsTvSeries.tagline}</em>
-            <h2>Overview</h2>
-            <p>{detailsTvSeries.overview}</p>
+          <div className={styles.summary}>
+            {detailsTvSeries.tagline && (
+              <em className={styles.tagline}>{detailsTvSeries.tagline}</em>
+            )}
+            {detailsTvSeries.overview && (
+              <>
+                <h2 className={`headingM ${styles.subtitle}`}>Overview</h2>
+                <p className={styles.overview}>{detailsTvSeries.overview}</p>
+              </>
+            )}
           </div>
-          <ol>
-            {detailsTvSeries.created_by.map((people) => (
-              <li key={people.id}>
-                <span>{people.name}</span>
-                <span>Creator</span>
-              </li>
-            ))}
-          </ol>
+          {detailsTvSeries.created_by.length > 0 && (
+            <ol className={styles.listCreators}>
+              {detailsTvSeries.created_by.map((people) => (
+                <li key={people.id} className={styles.cardCreator}>
+                  <span className={`headingS ${styles.namePeople}`}>
+                    {people.name}
+                  </span>
+                  <span className={styles.indicator}>Creator</span>
+                </li>
+              ))}
+            </ol>
+          )}
         </div>
       </section>
-      <section>
-        <div>
-          <p>
-            <span>Status</span>
-            <span>{detailsTvSeries.status}</span>
+      <section className={styles.sectionInfo}>
+        <h2 className={`headingM ${styles.subtitle}`}>Information</h2>
+        <div className={styles.containerInfo}>
+          <p className={styles.info}>
+            <span className={styles.data}>Status</span>
+            <span className={styles.valueData}>{detailsTvSeries.status}</span>
           </p>
-          <p>
-            <span>Type</span>
-            <span>{detailsTvSeries.type}</span>
+          <p className={styles.info}>
+            <span className={styles.data}>Type</span>
+            <span className={styles.valueData}>{detailsTvSeries.type}</span>
           </p>
-          <p>
-            <span>Original Language</span>
-            <span>
+          <p className={styles.info}>
+            <span className={styles.data}>Original Language</span>
+            <span className={styles.valueData}>
               {getLanguage(detailsTvSeries.original_language, listOfLanguages)}
             </span>
           </p>
         </div>
-        <div>
-          <p>Network</p>
-          <ul>
+        <div className={styles.containerInfo}>
+          <p className={styles.data}>Network</p>
+          <ul className={styles.listNetworks}>
             {detailsTvSeries.networks.map((network) => (
-              <li key={network.id}>
+              <li key={network.id} className={styles.itemNetwork}>
                 <Image
                   src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${network.logo_path}`}
                   alt={`logo nextwork ${network.name}`}
@@ -144,6 +159,7 @@ export default async function Page({ params }: Props) {
                   width={80}
                   height={30}
                   title={`logo nextwork ${network.name}`}
+                  className={styles.logoNetwork}
                 />
               </li>
             ))}
@@ -154,46 +170,52 @@ export default async function Page({ params }: Props) {
           target="_blank"
           rel="external"
           title="Visit Home Page"
+          className={styles.linkHomePage}
         >
-          <IconLink />
+          <IconLink className={styles.iconLink} />
         </Link>
       </section>
-      <section>
-        <h2>Seasons</h2>
-        {detailsTvSeries.seasons.map((season) => {
-          const date = new Date(season.air_date);
-          const monthTextLong = new Intl.DateTimeFormat("en-US", {
-            month: "long",
-          }).format(date);
-          return (
-            <div key={season.id}>
-              <div>
-                <Image
-                  src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${season.poster_path}`}
-                  alt={`poster season ${season.name}`}
-                  placeholder="blur"
-                  blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                    shimer(240, 140)
-                  )}`}
-                  width={130}
-                  height={195}
-                  title={`poster season ${season.name}`}
-                />
+      <section className={styles.sectionSeasons}>
+        <h2 className={`headingM ${styles.subtitle}`}>Seasons</h2>
+        <div>
+          {detailsTvSeries.seasons.map((season) => {
+            const date = new Date(season.air_date);
+            const monthTextLong = new Intl.DateTimeFormat("en-US", {
+              month: "long",
+            }).format(date);
+            return (
+              <div key={season.id} className={styles.cardSeason}>
+                <div>
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE}${season.poster_path}`}
+                    alt={`poster season ${season.name}`}
+                    placeholder="blur"
+                    blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                      shimer(240, 140)
+                    )}`}
+                    width={130}
+                    height={195}
+                    title={`poster season ${season.name}`}
+                    className={styles.posterSeason}
+                  />
+                </div>
+                <div className={styles.cardContent}>
+                  <h3 className={`headingS ${styles.nameSeason}`}>
+                    {season.name}
+                  </h3>
+                  <h4 className={`headingXS ${styles.metadatasSeason}`}>
+                    {date.getFullYear()} | {season.episode_count} Episodes
+                  </h4>
+                  <p className={styles.presentation}>
+                    {season.name} of {detailsTvSeries.name} premiered on{" "}
+                    {monthTextLong} {date.getDate()}, {date.getFullYear()}
+                  </p>
+                  <p className={styles.overviewSeason}>{season.overview}</p>
+                </div>
               </div>
-              <div>
-                <h3>{season.name}</h3>
-                <h4>
-                  {date.getFullYear()} | {season.episode_count} Episodes
-                </h4>
-                <p>
-                  {season.name} of {detailsTvSeries.name} premiered on{" "}
-                  {monthTextLong} {date.getDate()}, {date.getFullYear()}
-                </p>
-                <p>{season.overview}</p>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </section>
       <section>
         <h2>Casts</h2>
