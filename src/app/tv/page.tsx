e5c298ebc2c8f65abe65efec4f2e5c0d7ddbec93
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-07 11:23:17 -0300

    feat: adicionando metadadados página de generos de tv series

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -3,6 +3,35 @@ import Search from "@/components/Search";
 import styles from "./pagetv.module.css";
 import Link from "next/link";
 
+export const metadata = {
+  title: "Genres TV Series",
+  description:
+    "entertainment web application, which shows the most popular movies, series and favorite television programs, allowing you to obtain information.",
+  keywords: [
+    "Movies",
+    "Series",
+    "TV",
+    "entertainment",
+    "Genres",
+    "Genres Movies",
+    "Genres TV Series",
+    "TV Series",
+  ],
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
+  openGraph: {
+    title: "Entertainment web app",
+    description:
+      "entertainment web application, which shows the most popular movies, series and favorite television programs, allowing you to obtain information.",
+    url: "/movie",
+    type: "website",
+    siteName: "Entertainment web app",
+  },
+};
+
 export default async function Page() {
   const datas = await getGenresTvSeries();
 
@@ -10,7 +39,7 @@ export default async function Page() {
     <>
       <header className={styles.header}>
         <Search placeholder="Search for TV Series" />
-        <h1 className={`headingL ${styles.title}`}>Genres Movies</h1>
+        <h1 className={`headingL ${styles.title}`}>Genres TV Series</h1>
       </header>
       <main className={styles.main}>
         <ul className={styles.list}>
