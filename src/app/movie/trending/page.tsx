31fadae26a7fe1003becc2941bc32059495187e9
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-07 16:17:47 -0300

    feat: página de trending movies e estilização mobile-first, paginação de filmes de tendencia e página de loading para trending page

--- src/app/movie/trending/page.tsx
+++ src/app/movie/trending/page.tsx
@@ -1,3 +1,9 @@
+import { getTrendingMovies } from "@/api/tmdb";
+import styles from "./pagetrending.module.css";
+import Link from "next/link";
+import { renderCardMovie } from "@/components/SectionMovies";
+import List from "@/components/List";
+
 type Props = {
   params: { id: number };
   searchParams: {
@@ -5,6 +11,90 @@ type Props = {
   };
 };
 
-export default function Page({ searchParams }: Props) {
-  return <h1>Page trending movies page {searchParams["page"]}</h1>;
+export const metadata = {
+  title: "Trending Movies",
+  description:
+    "entertainment web application, which shows the most popular movies, series and favorite television programs, allowing you to obtain information.",
+  keywords: [
+    "Movies",
+    "Series",
+    "TV",
+    "entertainment",
+    "Genres",
+    "Genres Movies",
+    "Trending Movies",
+    "Trending",
+  ],
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
+  openGraph: {
+    title: "Trending Movies",
+    description:
+      "entertainment web application, which shows the most popular movies, series and favorite television programs, allowing you to obtain information.",
+    url: "/movie/trending?page=1",
+    type: "video.movie",
+    siteName: "Entertainment web app",
+  },
+};
+
+export default async function Page({ searchParams }: Props) {
+  let pageIndex = 1;
+  if (
+    searchParams["page"] &&
+    typeof searchParams["page"] === "string" &&
+    Number(searchParams["page"]) > 0 &&
+    Number(searchParams["page"]) < 500 //http 422 page must be less than or equal to 500
+  ) {
+    pageIndex = Number(searchParams["page"]);
+  }
+
+  const datas = await getTrendingMovies(pageIndex);
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={`headingL ${styles.title}`}>Trending Movies</h1>
+      </header>
+      <main
+        className={styles.wrapperList}
+        aria-live="polite"
+        aria-atomic="true"
+      >
+        <List
+          items={datas.results}
+          limitRenderingItems={20}
+          type="common"
+          renderItem={renderCardMovie}
+        />
+      </main>
+      <footer className={styles.containerButtons}>
+        {pageIndex > 1 && (
+          <Link
+            href={`/movie/trending?page=${pageIndex - 1}`}
+            rel="next"
+            title="Visit page previous movies"
+            className={styles.btnLink}
+          >
+            Previous
+          </Link>
+        )}
+        <p className={styles.indicator} aria-live="polite" aria-atomic="true">
+          {datas.page} of {datas.total_pages}
+        </p>
+        {pageIndex < datas.total_pages && (
+          <Link
+            href={`/movie/trending?page=${pageIndex + 1}`}
+            rel="next"
+            title="Visit page next movies"
+            className={styles.btnLink}
+          >
+            Next
+          </Link>
+        )}
+      </footer>
+    </div>
+  );
 }
