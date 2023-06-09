2ece3802fec30a12590acb4ef12b2a5b54253832
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-09 20:46:16 -0300

    feat: adicionando swr para busca de dados de uma api onde esta manipulado a rota de dados de paginação de um filme baseado em genero

--- src/app/api/movie/genre/[id]/route.ts
+++ src/app/api/movie/genre/[id]/route.ts
@@ -1,22 +1,15 @@
 import { getPageMoviesByGenre, getTrendingMovies } from "@/api/tmdb";
 import { NextResponse } from "next/server";
 
-type Params = {
-  request: Request;
-  params: { id: number };
-};
-
 //manipulador de rota para carregar dados de paginação de filmes baseado em generos com paramentros de pesquisa
 
 export async function GET(request: Request, {params}: {params: { id: number }}) {
-    const {searchParams} = new URL(request.url);
-    const pageIndex = searchParams.get("page");
-    const nameGenre = searchParams.get("name");
-    if(pageIndex && nameGenre && typeof Number(pageIndex) === "number" && Number(pageIndex) > 0 &&
-    Number(pageIndex) < 500 && typeof Number(params.id) === "number") { //http 422 page must be less than or equal to 500)
-        const datasPagination = await getPageMoviesByGenre(params.id, Number(pageIndex));
-        return NextResponse.json({datasPagination: datasPagination, nameGenre: nameGenre});    
-    }
-    const datasPagination = await getPageMoviesByGenre(params.id, 1);
-    return NextResponse.json({datasPagination: datasPagination, nameGenre: nameGenre});    
+  const {searchParams} = new URL(request.url);
+  const pageIndex = searchParams.get("page");
+  if(pageIndex && typeof Number(pageIndex) === "number" && Number(pageIndex) > 0 && Number(pageIndex) < 500 && typeof Number(params.id) === "number") { //http 422 page must be less than or equal to 500)
+    const datasPagination = await getPageMoviesByGenre(params.id, Number(pageIndex));
+    return NextResponse.json(datasPagination);    
+  }
+  const datasPagination = await getPageMoviesByGenre(params.id, 1);
+  return NextResponse.json(datasPagination);
 }
--- src/components/PaginationMoviesByGenre/index.tsx
+++ src/components/PaginationMoviesByGenre/index.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { getPageMoviesByGenre } from "@/api/tmdb";
 import Link from "next/link";
 import List from "../List";
 import { renderCardMovie } from "../SectionMovies";
 import styles from "./styles.module.css";
+import useSWR from "swr";
+import { ResponsePaginationMoviesByGenre } from "@/types";
+import SkeletonPagination from "../Skeletons/Pagination";
 
 type Props = {
   idGenre: number;
@@ -10,19 +14,43 @@ type Props = {
   nameGenre: string;
 };
 
-export default async function PaginationMoviesByGenre({
+async function fetcher(url: string) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(
+      "Failed to fetch datas by movies from genre, datas pagination"
+    );
+  }
+
+  const datas: Promise<ResponsePaginationMoviesByGenre> = response.json();
+
+  return datas;
+}
+
+export default /*async*/ function PaginationMoviesByGenre({
   idGenre,
   pageIndex,
   nameGenre,
 }: Props) {
-  const datasPagination = await getPageMoviesByGenre(idGenre, pageIndex);
+  /*const datasPagination = await getPageMoviesByGenre(idGenre, pageIndex);*/
+  const { data, error, isLoading } = useSWR(
+    `/api/movie/genre/${idGenre}?page=${pageIndex}`,
+    fetcher
+  );
+
+  if (isLoading) {
+    return <SkeletonPagination limitRenderingItems={20} />;
+  }
+
+  if (!data) throw new Error("Failed Fetch datas pagination movies from genre");
 
   return (
     <>
       <div className={styles.wrapperList} aria-live="polite" aria-atomic="true">
         <List
           mediaType="movie"
-          items={datasPagination.results}
+          items={data.results /*datasPagination.results*/}
           limitRenderingItems={20}
           type="common"
           renderItem={renderCardMovie}
@@ -43,9 +71,10 @@ export default async function PaginationMoviesByGenre({
           </Link>
         )}
         <p className={styles.indicator} aria-live="polite" aria-atomic="true">
-          {datasPagination.page} of {datasPagination.total_pages}
+          {/*datasPagination.page*/ data.page} of{" "}
+          {data.total_pages /*datasPagination.total_pages*/}
         </p>
-        {pageIndex < datasPagination.total_pages && (
+        {pageIndex < data.total_pages /*datasPagination.total_pages*/ && (
           <Link
             href={{
               pathname: `/movie/genre/${idGenre}`,
