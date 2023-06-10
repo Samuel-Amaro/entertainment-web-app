bca6e0b88436a708e8cd91cf3512458fe3f5afae
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-10 10:11:20 -0300

    feat: aplicando condicional para mostrar poster de movie ou tv serie somente se existir

--- src/components/CardCommon/index.tsx
+++ src/components/CardCommon/index.tsx
@@ -20,18 +20,20 @@ export default function CardCommon({
   return (
     <div className={styles.card}>
       <div className={styles.wrapperImage}>
-        <Image
-          src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE + posterPath}`}
-          alt={`poster ${mediaType} ${titleOrName}`}
-          /*width={240}
+        {posterPath && (
+          <Image
+            src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE + posterPath}`}
+            alt={`poster ${mediaType} ${titleOrName}`}
+            /*width={240}
           height={140}*/
-          placeholder="blur"
-          blurDataURL={`data:image/svg+xml;base64,${toBase64(
-            shimer(240, 140)
-          )}`}
-          className={styles.image}
-          fill={true}
-        />
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimer(240, 140)
+            )}`}
+            className={styles.image}
+            fill={true}
+          />
+        )}
         <ViewCard className={styles.hoverPoster} />
       </div>
       <DatasCard mediaType={mediaType} date={date} titleOrName={titleOrName} />
--- src/components/CardTrending/index.tsx
+++ src/components/CardTrending/index.tsx
@@ -21,16 +21,20 @@ export default function CardMovieTrending({
 }: PropsCardMovieTrending) {
   return (
     <div className={styles.card}>
-      <Image
-        src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE + posterPath}`}
-        alt={`poster ${mediaType} ${titleOrName}`}
-        /*width={240}
+      {posterPath && (
+        <Image
+          src={`${process.env.NEXT_PUBLIC_BASE_URL_IMAGE + posterPath}`}
+          alt={`poster ${mediaType} ${titleOrName}`}
+          /*width={240}
         height={140}*/
-        placeholder="blur"
-        blurDataURL={`data:image/svg+xml;base64,${toBase64(shimer(240, 140))}`}
-        className={styles.image}
-        fill={true}
-      />
+          placeholder="blur"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+            shimer(240, 140)
+          )}`}
+          className={styles.image}
+          fill={true}
+        />
+      )}
       <DatasCard
         mediaType={mediaType}
         date={date}
