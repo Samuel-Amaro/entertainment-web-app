8d90e9ef4fa5b6be7d0ae1a9b5ccf9238f5d1860
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-06-13 14:11:09 -0300

    feat: adicionando route handler para retornar uma lista de dados de uma determinado segmento de lista dinamico que corresponda ao pre-definidos, retorna dados de paginação de uma lista de tv series pre definida

---
+++ src/app/api/tv/list/[name]/route.ts
@@ -0,0 +1,33 @@
+import { getAiringTodayTv, getOnTheAirTv, getPopularTv } from "@/api/tmdb";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, {params}: {params: { name: string }}) {
+  const {searchParams} = new URL(request.url);
+  let pageIndex = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+  if(typeof pageIndex !== "number" || pageIndex <= 0 || pageIndex >= 500 || isNaN(pageIndex)) {
+    pageIndex = 1;    
+  }
+
+  switch(params.name) {
+    case 'airing-today': {
+      const datasPaginationListAiringToday = await getAiringTodayTv(pageIndex);
+      return NextResponse.json(datasPaginationListAiringToday);
+    }
+    case 'on-the-air': {
+      const datasPaginationListOnTheAir = await getOnTheAirTv(pageIndex);
+      return NextResponse.json(datasPaginationListOnTheAir);
+    }    
+    case 'popular': {
+      const datasPaginationListPopular = await getPopularTv(pageIndex);
+      return NextResponse.json(datasPaginationListPopular);
+    }
+    case 'top-rated': {
+      const datasPaginationListPopular = await getPopularTv(pageIndex);
+      return NextResponse.json(datasPaginationListPopular);
+    }
+    default: {
+      const datasPaginationListPopular = await getPopularTv(pageIndex);
+      return NextResponse.json(datasPaginationListPopular);
+    }
+  }
+}
